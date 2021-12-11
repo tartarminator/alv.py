1e52366d77b2e6bdd88a161e06057005d4b112e6
Alisher Vakilov <alishervakilov@gmail.com>
2021-12-11 12:53:15 +0500

    Trying to use git

--- ArgsAndKwargs.py
+++ ArgsAndKwargs.py
@@ -1,20 +1,20 @@
 # *Args and **Kwargs
-
+#
 # *args
-
+#
 # def func_with_args(*args):
 #     print(args)
 #
 #
 # func_with_args(2, 3, 15, 29)
-
-
+#
+#
 # def ten_percent_of_product(x, y, z):
 #     return (x * y * z)*0.1
 #
 #
 # print(ten_percent_of_product(10, 20, 7))
-
+#
 # def ten_percent_with_args(*args):
 #     product = 1
 #     for num in args:
@@ -23,8 +23,8 @@
 #
 #
 # print(ten_percent_with_args(10, 20, 7, 15))
-
-
+#
+#
 # def percent_with_args(percent, *args):
 #     product = 1
 #     for num in args:
@@ -33,15 +33,15 @@
 #
 #
 # print(percent_with_args(10, 200))
-
+#
 # **kwargs
-
+#
 # def func_with_kwargs(**kwargs):
 #     print(kwargs)
 #
 #
 # func_with_kwargs(first=1, second=2, third=3, fourth=4)
-
+#
 # def hello_with_kwargs(**kwargs):
 #     if 'name' in kwargs:
 #         print('Hello, nice to meet you, {}'.format(kwargs['name']))
@@ -51,7 +51,7 @@
 #
 # hello_with_kwargs(gender='male', age=50, name='Alish')
 # hello_with_kwargs(gender='male', age=50)
-
+#
 # def hello_with_greeting_and_kwargs(greeting, **kwargs):
 #     if 'name' in kwargs:
 #         print('{}, nice to meet you, {}'.format(greeting, kwargs['name']))
@@ -61,7 +61,7 @@
 #
 # hello_with_greeting_and_kwargs('Hola', gender='male', age=50, name='Alish')
 # hello_with_greeting_and_kwargs('Guten Abend', gender='male', age=50)
-
+#
 #
 # def func_with_args_and_kwargs(*args, **kwargs):
 #     print('{},bring me {} and {} and don\'t spoil my {}.'.format(args[2], kwargs['food'], kwargs['drink'],
@@ -69,8 +69,8 @@
 #
 #
 # func_with_args_and_kwargs('Jimmy', 'Singh', 'Indus', drink='borman', food='fish', cloth='jeans')
-
-
+#
+#
 # def is_cat_there(*args):
 #     if 'cat' in str(args).lower():
 #         return True
@@ -79,8 +79,8 @@
 #
 #
 # print(is_cat_there('Cat', 'dog', 'bird', 'snake'))
-
-
+#
+#
 # def is_item_here(item, *args):
 #     if item in args:
 #         return True
