87e3184c5540a7dab9bc3915643bd5dbc97d6e95
Alisher Vakilov <alishervakilov@gmail.com>
2021-12-14 17:29:52 +0500

    Trying to use git

--- zips/Scope/OopClasses.py
+++ zips/Scope/OopClasses.py
@@ -13,14 +13,17 @@
 # print(type(object_of_my_class))
 
 class Car:
-    def __init__(self, model, color, year):
+    def __init__(self, model, color, year, is_crashed):
         self.model = model
         self.color = color
         self.year = year
+        self.is_crashed = is_crashed
 
 
-mazda_car = Car(model='Mazda CX7', color='red', year=2018)
-print(mazda_car.model, mazda_car.color, mazda_car.year)
+mazda_car = Car(model='Mazda CX7', color='red', year=2018, is_crashed=True)
+print(mazda_car.model, mazda_car.color, mazda_car.year, mazda_car.is_crashed)
 
-bmw_car = Car(model='BMW Z-800', color='blue', year=2020)
-print(bmw_car.model, bmw_car.color, bmw_car.year)
+bmw_car = Car(model='BMW Z-800', color='blue', year=2020, is_crashed=False)
+print(bmw_car.model, bmw_car.color, bmw_car.year, bmw_car.is_crashed)
+print(type(bmw_car))
+
