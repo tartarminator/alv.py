8f7c33e653f7164b1bac1c6bb753ed1b732af2e0
Alisher Vakilov <alishervakilov@gmail.com>
2021-12-15 15:50:51 +0500

    Trying to use git

--- inheritance_and_polymorphism.py
+++ inheritance_and_polymorphism.py
@@ -9,14 +9,21 @@ class Car:
         self.color = color
         self.year = year
         self.is_crashed = is_crashed
+        print('Car is created.')
 
     def drive(self, city):
-        print(self.name + ' is driving to ' + city)
+        print(self.model + ' is driving to ' + city)
 
     def change_color(self, new_color):
         self.color = new_color
 
-class Truck(Car)
+
+class Truck(Car):
+    def __init__(self, model, color, year, is_crashed):
+        Car.__init__(self, model, color, year, is_crashed)
+        print('Truck is created.')
 
 
+volvo_truck = Truck('Volvo', 'grey', 2010, False)
+volvo_truck.drive('London')
 
